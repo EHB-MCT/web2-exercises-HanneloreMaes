178718a0bb2d66fd81158dc62562413eab7ac371
HanneloreMaes <71761907+HanneloreMaes@users.noreply.github.com>
2021-09-30 15:14:59 +0200

    Oef 1 -> werkend (ex 1-3)

--- S1/1/index.js
+++ S1/1/index.js
@@ -1,26 +1,52 @@
 "use strict";
 
-const form = {
-    initfields(){
-        const formEvent = document.getElementById("form");
-        // const formEvent = document.getElementsByClassName('buttonSub');
-        formEvent.addEventListener('submit', this.submitHandler)
-    },
+window.onload = function(){
+    console.log('Succes');
 
-    submitHandler(e){
+    let formEvent = document.getElementById('form').addEventListener('submit', e => {
         e.preventDefault();
-        console.log('Succes');
-        
+
         let name = document.getElementById("nameInputs").value;
         let email = document.getElementById("emailInputs").value;
         let order = document.getElementById("orderInputs").value;
 
-        document.getElementById("input").innerHTML= getMessage(name,email,order);
-    },
-    getMessage(b,c,d){
-        let a = `Order for ${b} is ordered. Order: ${c}. Confirmation is send to ${d}.`
-        
-        return a
-    }
+        let orderScript = {
+            name, email, order
+        }
+
+        document.getElementById('messages').innerHTML = messagePrint(orderScript);
+
+    })
 
 }
+
+function messagePrint(orderScript){
+    return `<p> The order for the customer ${orderScript.name} is the following: ${orderScript.order}. The customer may be notified by email: ${orderScript.email}. </p>`
+}
+
+
+
+// const form = {
+//     initfields(){
+//         const formEvent = document.getElementById("form");
+//         // const formEvent = document.getElementsByClassName('buttonSub');
+//         formEvent.addEventListener('submit', this.submitHandler)
+//     },
+
+//     submitHandler(e){
+//         e.preventDefault();
+//         console.log('Succes');
+        
+//         let name = document.getElementById("nameInputs").value;
+//         let email = document.getElementById("emailInputs").value;
+//         let order = document.getElementById("orderInputs").value;
+
+//         document.getElementById("input").innerHTML= getMessage(name,email,order);
+//     },
+//     getMessage(b,c,d){
+//         let a = `Order for ${b} is ordered. Order: ${c}. Confirmation is send to ${d}.`
+        
+//         return a
+//     }
+
+// }
