13bbf1c76ee54517ab0052c019af4bff3b2ae8b5
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-10-05 15:54:25 +0200

    Ex 1 -> foto's en info inladen

--- S2/index.js
+++ S2/index.js
@@ -5,19 +5,19 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
     .then(data => {
         console.log('Succes', data)
         //console.log('test', data.results)
-        let pokémon = data.results
-        pokémon.forEach(e => {
-            //console.log('forEach', e)
-            fetch(`https://pokeapi.co/api/v2/pokemon/1/`)
+        let pokémon = data.results        
+        pokémon.forEach(poke => {
+            //console.log('forEach', poke)
+            fetch(poke.url)
                 .then (response2 => response2.json())
                 .then(data2 => {
                     console.log('Succes 2', data2);
-                    console.log('Types', data2.types);
+                    //console.log('Types', data2.types);
                     let container = document.getElementById('blokske');
                     let htmlString = `
-                        <img src="" alt="pokémonImg">
+                        <img src="${data2.sprites.front_default}" alt="pokémonImg">
                         <h2>${data2.name}</h2>
-                        <p>Type: ${data2.types}</p>
+                        <p>Type: ${data2.types[0].type.name}</p>
                         <button type="submit"></button>`;
                     
                         container.insertAdjacentHTML('beforeEnd', htmlString);
