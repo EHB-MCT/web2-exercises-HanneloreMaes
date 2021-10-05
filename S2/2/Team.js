545eb62ff0ded00b270aa6420721b711bd3ec9ea
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-10-05 17:22:14 +0200

    Ex 2 -> Alles js

---
+++ S2/2/Team.js
@@ -0,0 +1,16 @@
+"use strict";
+
+class Team {
+    constructor(Name, Trainer){
+        this.teamname = Name,
+        this.trainer = Trainer,
+        this.roster = []
+    }
+    describe(){
+        let description = `Goodluck ${this.trainer} with ${this.teamname} and these pokémons ${[...this.roster]}. May the best team win!`;
+
+        return description
+    }
+}
+
+export default Team;
