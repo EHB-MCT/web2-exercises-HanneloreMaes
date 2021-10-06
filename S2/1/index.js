884af6b864a338408d50e35d7499f6b69b3b6532
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-10-06 11:48:28 +0200

    Ex 3 -> Button function +- af

--- S2/1/index.js
+++ S2/1/index.js
@@ -1,14 +1,13 @@
 "use strict";
 
-import * as Team from '../2/Team.js';
+import Team from '../2/Team.js';
 
 let team1 = new Team();
 
-window.onload = function(){
     fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
     .then(response => response.json())
     .then(data => {
-        console.log('Succes', data)
+        //console.log('Succes', data)
         //console.log('test', data.results)
         let pokémon = data.results        
         pokémon.forEach(poke => {
@@ -16,35 +15,38 @@ window.onload = function(){
             fetch(poke.url)
                 .then (response2 => response2.json())
                 .then(data2 => {
-                    console.log('Succes 2', data2);
+                    //console.log('Succes 2', data2);
 
-                    let hello = data2.types;
-                    console.log('giyefgyfs', hello);
-                    let types = hello.map(obj => obj.type.name);
-                    console.log('Types', types);
+                    let pokémonData = data2.types;
+                    //console.log('giyefgyfs', hello);
+                    let types = pokémonData.map(obj => obj.type.name);
+                    //console.log('Types', types);
                     
                     let container = document.getElementById('blokske');
                     let htmlString = `
                         <h2>${data2.name}</h2>
                         <img src="${data2.sprites.front_default}" alt="pokémonImg">
                         <p>Type: ${types}</p>
-                        <button type="submit" id="addPokémon">Take me home :)</button>`;
+                        <button type="button" class="btn" id="${data2.id}">Take me home :)</button>`;
                     
                         container.insertAdjacentHTML('beforeEnd', htmlString);
+                
+
+                    let button = document.querySelectorAll('.btn');
+                    button.forEach(item => {
+                        item.addEventListener('click', e =>{
+                            e.preventDefault();
+                            let id = e.target.id;
+                            console.log('Hello', id);
+                        });
+                    })
+    
                 })
         })
         
     })
 
-    let button = document.getElementById('addPokémon');
-    button.forEach(item => {
-        item.addEventListener('click', e =>{
-            e.preventDefault();
-
-            let id = e.target.id;
-        });
-    })
-}
+    
 
 refreshTeam();
 
