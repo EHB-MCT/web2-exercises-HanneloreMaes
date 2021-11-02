96b2a5c16f5597b49726a1681d332103fd30d10b
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-11-02 17:38:23 +0100

    Eindwerk -> Nieuwe test locatie

--- Eindwerk/js/getLocation.js
+++ Eindwerk/js/getLocation.js
@@ -1,35 +1,49 @@
 "use strict"; 
 
+
+
+
+
 class LocationTracker {
   constructor() {
-    this._location = null;
+    this.test = 1232132;
+  }
+  async getData(){
+    //this._location = 200;
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
         fetch(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=cf2adb444cd94e24ab41876ff1f5272f`)
-          .then(response => response.json())
-          .then(data => {
+        .then(response => response.json())
+        .then(data => {
             console.log('Succes', data.results);
             let locatie = data.results;
             let town = locatie.map(obj => obj.components.town);
             //console.log('Succes2', town[0]);
-            this.location = {
-              city: town,
-            };
-            console.log('this.location', this.location);
-            let hellow = this.location.city;
+            // let location = {
+            //   city: town,
+            // };
+            console.log('this.location', town);
+            this.test = town;
+            console.log("h", this.test)
+            return this.test
             //console.log('Top', hellow);
-
-            return hellow;
+            // this._location = hellow;
+            // console.log('Kat', this._location)
+          
         });
+        console.log(this.test)
         //console.log('Hellow voor if', this.hellow);
       });
     } else {
       //console.log( "Geolocation is not supported by this browser.");
     }
-    this._location = this.hellow;
+    
     //console.log('Locatie na if', this._location);
+  }
+  getTest() {
+    return this.test;
   }
 }
 
